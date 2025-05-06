ef0a98c6d02f1e589fc2859ff378d7325d494c55
Chiu <zsh0908@outlook.com>
2025-05-06 14:54:00 +0800

    修复 重复下载OCR模型导致答题过慢的Bug

--- util/ocr.py
+++ util/ocr.py
@@ -3,14 +3,14 @@ import requests
 import numpy as np
 import cv2
 
+# 全局只初始化一次
+reader = easyocr.Reader(['ch_sim', 'en'], gpu=True)  # 有GPU可以设为True，提高速度
 
 def ocr_form_url_image(url):
     # 用requests下载图片
     response = requests.get(url)
     image_array = np.asarray(bytearray(response.content), dtype=np.uint8)
     img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-    # 初始化EasyOCR
-    reader = easyocr.Reader(['ch_sim', 'en'])
     # 用numpy数组识别
     result = reader.readtext(img)
 
